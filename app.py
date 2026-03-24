1a4a66b947a86ff96df27fa5152a3a0419c8acee
24pca025-bit <24pca025@avinuty.ac.in>
2026-03-24 13:21:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if "patient_name" not in st.session_state:
 if st.session_state.page == "home":
 
     st.markdown(
-        '<div class="main-title">Lung Cancer Prediction Using Machine Learning</div>',
+        '<div class="main-title">LUNG CANCER PREDICTION </div>',
         unsafe_allow_html=True
     )
 
